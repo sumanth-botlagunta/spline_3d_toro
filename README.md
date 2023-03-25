c449faa69d75033fadbc9e632e9632a036f11265
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-03-25 22:28:50 +0530

    spline 3dmodel

---
+++ README.md
@@ -0,0 +1,5 @@
+## This is a 3d model created in spline and inserted in html
+
+[3d model](https://app.spline.design/file/6274971b-16bc-4d6e-bca1-58753c36bfa1)
+
+![Project Completed](https://img.shields.io/badge/Project%20Completed-Yes-blue)
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/Nkr9GHo1SFKMGM2O/scene.splinecode');
+
+
+
+
+
